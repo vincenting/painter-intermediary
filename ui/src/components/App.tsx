49f1be15be76b55d41c07bc055a4ad4e089e5b49
Vincent Ting <1445522+vincenting@users.noreply.github.com>
2022-12-29 17:03:40 +0800

    faet: add basic UI and login page

---
+++ ui/src/components/App.tsx
@@ -0,0 +1,72 @@
+// Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import DamlHub, {
+  isRunningOnHub,
+  usePublicParty,
+  usePublicToken
+} from "@daml/hub-react";
+import { createLedgerContext } from "@daml/react";
+import React from "react";
+import Credentials from "../Credentials";
+import LoginScreen from "./LoginScreen";
+
+// Context for the party of the user.
+export const userContext = createLedgerContext();
+// Context for the public party used to query user aliases.
+// On Daml hub, this is a separate context. Locally, we have a single
+// token that has actAs claims for the user’s party and readAs claims for
+// the public party so we reuse the user context.
+export const publicContext = isRunningOnHub()
+  ? createLedgerContext()
+  : userContext;
+
+/**
+ * React component for the entry point into the application.
+ */
+// APP_BEGIN
+const App: React.FC = () => {
+  const [credentials, setCredentials] = React.useState<
+    Credentials | undefined
+  >();
+  if (credentials) {
+    const PublicPartyLedger: React.FC = ({ children }) => {
+      const publicToken = usePublicToken();
+      const publicParty = usePublicParty();
+      if (publicToken && publicParty) {
+        return (
+          <publicContext.DamlLedger
+            token={publicToken.token}
+            party={publicParty}>
+            {children}
+          </publicContext.DamlLedger>
+        );
+      } else {
+        return <h1>Loading ...</h1>;
+      }
+    };
+    const Wrap: React.FC = ({ children }) =>
+      isRunningOnHub() ? (
+        <DamlHub token={credentials.token}>
+          <PublicPartyLedger>{children}</PublicPartyLedger>
+        </DamlHub>
+      ) : (
+        <div>{children}</div>
+      );
+    return (
+      <Wrap>
+        <userContext.DamlLedger
+          token={credentials.token}
+          party={credentials.party}
+          user={credentials.user}>
+          hello world
+        </userContext.DamlLedger>
+      </Wrap>
+    );
+  } else {
+    return <LoginScreen onLogin={setCredentials} />;
+  }
+};
+// APP_END
+
+export default App;
